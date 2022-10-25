2e92104890640324783f27f29571b33fe0bb3eb1
liudmila05 <ol.liuda.94@gmail.com>
2022-10-25 21:35:27 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const newLightBox = document.querySelector(".gallery");
+const images = galleryItems
+    .map(
+        (image) =>
+            `<a class="gallery__item" href="${image.original}">
+                <img class="gallery__image"    
+                src="${image.preview}"
+                alt="${image.description}"
+                title="${image.description}"
+                />
+            </a>`
+    )
+    .join("");
+
+newLightBox.innerHTML = images;
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
 console.log(galleryItems);
