b8981379cd9c2f1e0b44f363af45e9fd0929e49f
liudmila05 <ol.liuda.94@gmail.com>
2022-10-27 22:09:54 +0300

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener(
+    'input',
+    throttle(e => {
+        const objectToSave = { email: email.value, message: message.value };
+        localStorage.setItem(localStorageKey, JSON.stringify(objectToSave));
+    }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(localStorageKey);
+});
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+    };
+};
+
+const storageData = load(localStorageKey);
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+};
