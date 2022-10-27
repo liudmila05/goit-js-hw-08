bfec9a64708cfec94ba356680e4f50e60d1b8c06
liudmila05 <ol.liuda.94@gmail.com>
2022-10-27 21:33:56 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const localStorageKey = localStorage.getItem("videoplayer-current-time");
+console.log(localStorageKey);
+
+
+player.setCurrentTime(localStorageKey).then(function () {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(function (time) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time.seconds));
+ 
+}, 1000));
